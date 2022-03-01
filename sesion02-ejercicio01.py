5c578ebb01f065a75961a172d8011b875c52ca8c
javiergemar <javigemar@gmail.com>
2022-03-01 08:40:30 +0100

    Modificado el ejercicio para sea tal cual pide el enunciado

--- sesion02-ejercicio01.py
+++ sesion02-ejercicio01.py
@@ -1,6 +1,10 @@
+# En este ejercicio tendrás que crear una variable a la cual asignarás
+# una cadena de texto y la imprimirás por consola, a su vez, tendrás que
+# imprimir también el tipo de la variable por consola.
+
 texto = input('Inserta una cadena de caracteres: ')
-print('La cadena de texto introducida es: ' + texto)
+print('La cadena de texto introducida es: ', texto)
 tipoVariable = type(texto)
 tipoVariable = str(tipoVariable)
-print('El tipo de dato es ' + tipoVariable)
+print('El tipo de dato es ',  tipoVariable)
 
