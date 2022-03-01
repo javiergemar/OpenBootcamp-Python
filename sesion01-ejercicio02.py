934d530b61794aaa1b3d57a60a3d0b16bed8ddee
javiergemar <javigemar@gmail.com>
2022-03-01 08:26:14 +0100

    Modificado el ejercicio para sea tal cual pide el enunciado

--- sesion01-ejercicio02.py
+++ sesion01-ejercicio02.py
@@ -1,9 +1,4 @@
-nombre = input("¿Cómo te llamas? ")
-tiempo = input("¿Cuántos meses llevas en OB? ")
-tiempo = int(tiempo)
-pendiente = 12 - tiempo
-pendiente = str(pendiente)
-print("Hola, eres " + nombre)
-print("Te quedan " + pendiente + " meses para acabar tu contrato, ¡¡¡ ESPABILA !!!")
-print("Veo que estás en el curso de Python")
-print("Todavía te queda mucho por aprender")
+# Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
+
+saludo = 'Hola mundo!'
+print(saludo)
