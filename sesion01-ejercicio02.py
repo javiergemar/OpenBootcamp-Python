afa51e4c0cf7dd774139d9795d42d788ea7c0e03
javiergemar <javigemar@gmail.com>
2022-03-02 07:32:55 +0100

    Modificado el enunciado a dos líneas

--- sesion01-ejercicio02.py
+++ sesion01-ejercicio02.py
@@ -1,4 +1,5 @@
-# Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
+# Desde la consola de python almacena la cadena
+# “Hola mundo!” en una variable y muéstrala.
 
 saludo = 'Hola mundo!'
 print(saludo)
