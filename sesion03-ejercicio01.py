dbb40ac49ccab255504d12f1732d4d159d75db9d
javiergemar <javigemar@gmail.com>
2022-03-07 06:10:22 +0100

    Ejercicio declarando variables e imprimiendo por pantalla

---
+++ sesion03-ejercicio01.py
@@ -0,0 +1,31 @@
+# Para este ejercicio tenéis que crear en la consola de python variables
+# que representen los siguientes datos de un contacto:
+#
+# - Nombre
+# - Apellidos
+# - Edad
+# - Email
+# - Teléfono
+# - Casado (verdadero o falso)
+# - Con Hijos (verdadero o falso)
+# - Lista de amigos
+# - Películas vistas (diccionario con clave y valor. El valor será el título de la película)
+
+nombre = ''
+apellidos = ''
+edad = int()
+email = ''
+telefono = int()
+casado = False
+tieneHijos = False
+amigos = []
+pelis = {'identificador': 'tituloPeli'}
+print(nombre)
+print(apellidos)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(tieneHijos)
+print(amigos)
+print(pelis)
