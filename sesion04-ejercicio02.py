214fcbafb5129dc2a1512beb8de84f2f2380e221
javiergemar <javigemar@gmail.com>
2022-03-08 07:22:31 +0100

    Mostrar si es número par o no

---
+++ sesion04-ejercicio02.py
@@ -0,0 +1,24 @@
+# Escribe un programa capaz de mostrar todos los números
+# impares desde un número de inicio y otro final.
+
+# Primero lo vamos a realizar con un if
+
+numInicial = int(input('Introduce el número inicial: '))
+numFinal = int(input('Introduce el número final: '))
+
+for i in range(numInicial,numFinal):
+    if i % 2 == 0:
+        print(i, 'es un número par')
+    else:
+        print(i, 'no es un número par')
+
+# Ahora realizado con un while
+
+contador = numInicial
+
+while contador <= numFinal:
+    if contador % 2 == 0:
+        print(contador, 'es un número par')
+    else:
+        print(contador, 'no es un número par')
+    contador += 1
