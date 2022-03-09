67d8298f07f1e55758e2f8bbd270f784ca276c72
javiergemar <javigemar@gmail.com>
2022-03-09 06:12:47 +0100

    Calculadora IMC

--- sesion03-ejercicio02.py
+++ sesion03-ejercicio02.py
@@ -1,8 +1,10 @@
-# Escribe un programa en la consola de python que pida al usuario
-# su peso (en kg) y estatura (en metros), calcule el índice de masa
-# corporal y lo almacene en una variable, e imprima por pantalla la
-# frase Tu índice de masa corporal es <imc> donde <imc> es el índice
-# de masa corporal calculado redondeado con dos decimales.
+'''
+Escribe un programa en la consola de python que pida al usuario
+su peso (en kg) y estatura (en metros), calcule el índice de masa
+corporal y lo almacene en una variable, e imprima por pantalla la
+frase Tu índice de masa corporal es <imc> donde <imc> es el índice
+de masa corporal calculado redondeado con dos decimales.
+'''
 
 print('Bienvenido a la calculadora de IMC')
 peso = int(input('¿Cuál es tu peso en Kg? '))
