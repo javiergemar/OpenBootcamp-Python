fe68951371a34863d2930ddf1c45b602c51770c9
javiergemar <javigemar@gmail.com>
2022-03-09 07:34:43 +0100

    Muestra los números de 100 al 1

--- sesion04-ejercicio03.py
+++ sesion04-ejercicio03.py
@@ -5,3 +5,13 @@ los números del 1 al 100 en orden inverso.
 
 numeros = list(range(100, 0, -1))
 print(numeros)
+
+# Con buble for
+delUnoAlCien = list(range(1, 101))
+for i in reversed(delUnoAlCien):
+    print('Número', i)
+
+# Otro bucle for
+delUnoAlCien = list(range(1, 101))
+for i in range(len(delUnoAlCien) -1, -1, -1):
+    print('Número', delUnoAlCien[i])
