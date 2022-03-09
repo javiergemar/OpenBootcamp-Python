4952ab65b2e4c9152dae90f886c7ecb699ba4b65
javiergemar <javigemar@gmail.com>
2022-03-09 06:10:24 +0100

    Crear variable e imprimir valor y tipo de dato

--- sesion02-ejercicio01.py
+++ sesion02-ejercicio01.py
@@ -1,6 +1,8 @@
-# En este ejercicio tendrás que crear una variable a la cual asignarás
-# una cadena de texto y la imprimirás por consola, a su vez, tendrás que
-# imprimir también el tipo de la variable por consola.
+'''
+En este ejercicio tendrás que crear una variable a la cual asignarás
+una cadena de texto y la imprimirás por consola, a su vez, tendrás que
+imprimir también el tipo de la variable por consola.
+'''
 
 texto = input('Inserta una cadena de caracteres: ')
 print('La cadena de texto introducida es: ', texto)
