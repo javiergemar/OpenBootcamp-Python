1fef793344b4d6d77cf45bd827d760e44d6fc0be
javiergemar <javigemar@gmail.com>
2022-03-13 17:50:07 +0100

    Probando lo aprendido en la sesión 5

--- sesion05-ejemplos.py
+++ sesion05-ejemplos.py
@@ -1,22 +1,23 @@
-texto = 'El resultado de la '           # variable de ámbito global
+texto = 'El resultado de la '                           # variable de ámbito global
 textoAlternativo = 'La operación da como resultado: '
 
-def matematicas(a, b):
-    def suma(a, b):
-        return a + b
-    def resta(a, b):
-        return a -b
+def matematicas(a = 8, b = 4, c = 2, d = 1):            # le indicamos que los parámetros sean opcionales
+    def suma(a, b, c, d):
+        return a + b + c +d
+    def resta(a, b, c, d):
+        return a - b -c -d
     texto = 'Resultado de'              # la variable local prevalece sobre la variable global
     global textoAlternativo             # la variable de ámbito global sobrescribe la de ámbito local
-    resultadoSuma = suma (a, b)
+    resultadoSuma = suma (a, b, c, d)
     print(texto, 'suma es: ', resultadoSuma)
     print(textoAlternativo, resultadoSuma)
 
-    resultadoResta = resta(a, b)
+    resultadoResta = resta(a, b, c, d)
     print(texto, 'resta es: ', resultadoResta)
     print(textoAlternativo, resultadoResta)
 
-    def masoperaciones(a, b):
+    def masoperaciones(*args):
+        resultado = =
         def multi(a, b):
             return a * b
 
@@ -32,26 +33,4 @@ def matematicas(a, b):
     masoperaciones(a, b)
 
 
-matematicas(4, 2)
-
-
-'''
-def matematicas(a, b):
-    def suma(a, b):
-        print(a + b)
-
-    def resta(a, b):
-        print(a - b)
-
-    def masoperaciones(a, b):
-        def multiplica(a, b):
-            print(a * b)
-
-        multiplica(a, b)
-
-    suma(a, b)
-    resta(a, b)
-    masoperaciones(a, b)
-
-matematicas(5, 3)
-'''
+matematicas()
