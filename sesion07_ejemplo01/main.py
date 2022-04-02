1fd44982fda025f1f1e6abc9e940fb9205abc17d
Javier Gemar <javigemar@gmail.com>
2022-04-02 13:37:44 +0200

    Curso Python - Ejemplo de paquetes dentro de paquetes

--- sesion07_ejemplo01/main.py
+++ sesion07_ejemplo01/main.py
@@ -1,8 +1,42 @@
-import operaciones
+import operaciones.restador.resta
+import operaciones.sumador.suma
 
-
-
-
+def main():
+    resta = operaciones.restador.resta.resta(5, 3)
+    suma = operaciones.sumador.suma.suma(5, 3)
+    print(resta)
+    print(suma)
 
 if __name__ == '__main__':
     main()
+
+# TAMBIÉN SE PODRÍA HACER DE LA SIGUIENTE MANERA ASIGNANDO ALIAS
+
+'''
+import operaciones.restador.resta as orr
+import operaciones.sumador.suma as oss
+
+def main():
+    resta = orr.resta(5, 3)
+    suma = oss.suma(5, 3)
+    print(resta)
+    print(suma)
+
+if __name__ == '__main__':
+    main()
+'''
+
+# OTRA FORMA SERÍA IMPORTANDO LOS MÓDULOS DE LOS PAQUETES DE LA SIGUIENTE FORMA
+
+'''
+from operaciones import restador, sumador
+
+def main():
+    resta = restador.resta(5, 3)        # ahora para llamar a la función solo tenemos 
+    suma = sumador.suma(5, 3)           # que poner el nombre del módulo antes de la función
+    print(resta)
+    print(suma)
+
+if __name__ == '__main__':
+    main()
+'''
---
+++ sesion07_ejemplo01/operaciones/restador/resta.py
@@ -0,0 +1,5 @@
+def resta(a, b):
+    return a - b
+
+def minus(a, b):
+    return resta(a, b)           # sería igual a: return a - b
