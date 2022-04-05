1a99636ddf9db6a46306f2574c83e49e4c7b81e1
Javier Gemar <javigemar@gmail.com>
2022-04-05 07:25:42 +0200

    Sesión 7 - Paquetes

---
+++ sesion07_paquetes/main_paquetes.py
@@ -0,0 +1,11 @@
+import operaciones.suma             # Importamos el módulo suma dentro del paquete operaciones
+
+
+def main():
+    mp = operaciones.suma.Multiplicador()       # Creamos un objeto de la clase Multiplicador()
+    print(mp.multiplica(5, 5))
+
+
+
+if __name__ == '__main':
+    main()
