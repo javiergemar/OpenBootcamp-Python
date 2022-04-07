d3a1f119a0496268068e1991688d9be27b9b9780
Javier Gemar <javigemar@gmail.com>
2022-04-07 06:27:11 +0200

    Sesión 7 - Paquetes dentro de paquetes

---
+++ sesion07_paquetes_en_paquetes/operaciones/sumador/suma.py
@@ -0,0 +1,24 @@
+def suma(a, b):
+
+    '''
+    Este texto lo coge PyCharm (y posiblemente otros IDEs) para mostrar la
+    ayuda en pantalla sobre esta función.
+    '''
+
+    return a + b
+
+def add(a, b):
+    return suma(a, b)
+
+class Sumatorio():
+    def sumaEnClase(self, a, b):
+        '''
+        Este texto lo coge PyCharm (y posiblemente otros IDEs) para mostrar la
+        ayuda en pantalla sobre esta función.
+        '''
+        return a + b
+
+
+
+    def addEnClase(self, a, b):
+        return a + b
