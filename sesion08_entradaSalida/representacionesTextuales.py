f3304e164bb57b3845f8bd7434c69488b09c5520
Javier Gemar <javigemar@gmail.com>
2022-04-20 06:34:50 +0200

    Sesión 8 - Representaciones textuales de cadenas

---
+++ sesion08_entradaSalida/representacionesTextuales.py
@@ -0,0 +1,12 @@
+num = 511
+print(type(num))
+
+numtxt = str(num)               # Convertimos num a cadena con str()
+print(type(numtxt))
+
+print(repr(num))                #
+print(numtxt)
+
+class Juguete():
+    nombre = ''
+    precio = 0.0
