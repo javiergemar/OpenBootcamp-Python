1411f4f337ce36d9f74411ee3792d029e5db5c9b
Javier Gemar <javigemar@gmail.com>
2022-04-21 06:55:55 +0200

    Sesión 8 - Jugando con strings

---
+++ sesion08_entradaSalida/jugandoConStrings.py
@@ -0,0 +1,6 @@
+import pprint
+
+print('\n* * * * * Jugando con cadenas de texto para mejorar las salidas * * * * *\n')
+
+pprint.pprint(dir(''))                                      # Listamos los métodos que puedo usar dentro de una cadena de texto
+
