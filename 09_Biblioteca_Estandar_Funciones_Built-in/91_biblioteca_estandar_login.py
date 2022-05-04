7abaee08da2fd84c24d6eeaad591b8f7321e518a
Javier Gemar <javigemar@gmail.com>
2022-05-04 07:33:11 +0200

    Sesión 9 - Biblioteca estándar y funciones built-in

---
+++ 09_Biblioteca_Estandar_Funciones_Built-in/91_biblioteca_estandar_login.py
@@ -0,0 +1,2 @@
+print('* * * * * Ficheros de registro * * * * *')
+
---
+++ 09_Biblioteca_Estandar_Funciones_Built-in/91_biblioteca_estandar_multihilo.py
@@ -0,0 +1,29 @@
+print('* * * * * Programación multihilo * * * * *')
+
+import _thread
+import time
+
+def horaActual(nombre_thread, tiempo_de_espera):
+    count = 0
+
+    while count < 5:
+        time.sleep(tiempo_de_espera)
+        count += 1
+        print(f'hilo: {nombre_thread} - {time.ctime(time.time())}')
+
+_thread.start_new_thread(horaActual, ('thread_uno', 1))
+_thread.start_new_thread(horaActual, ('thread_dos', 2))
+
+'''
+Cuando se ejecuta código multihilo tenemos que darle tiempo al programa para que se ejecute,
+es decir, tenemos que bloquear al programa para que se ejecute nuestro código paralelo.
+'''
+
+print('He disparado ya los hilos')
+
+while True:             # Podríamos hacerlo con un bucle
+    pass
+
+'''
+time.sleep(1)           # Otra forma de hacerlo dándole un segundo
+'''
