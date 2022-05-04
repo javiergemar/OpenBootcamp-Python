96b7321351a6ea3a921b12d3dc881e250f73023f
Javier Gemar <javigemar@gmail.com>
2022-05-04 07:38:51 +0200

    Sesión 9 - Biblioteca estándar y funciones built-in

---
+++ 09_Biblioteca_Estandar_Funciones_Built-in/pruebas.py
@@ -0,0 +1,13 @@
+numero = 3
+texto = 'El perro de San Roque'
+conDecimal = 1.5
+
+def suma(a, b):
+    return a + b
+
+print(f'El número es {suma(numero, numero)} y el texto es {texto} y tiene {conDecimal}')
+
+def resta():
+    print(3 -2)
+
+print(resta)
--- pruebas.py
+++
@@ -1,8 +0,0 @@
-numero = 3
-texto = 'El perro de San Roque'
-conDecimal = 1.5
-
-def suma(a, b):
-    return a + b
-
-print(f'El número es {suma(numero, numero)} y el texto es {texto} y tiene {conDecimal}')
