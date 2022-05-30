b4757e0e7123a34b5fdcebebb488c3ab9e219d18
Javier Gemar <javigemar@gmail.com>
2022-05-30 09:36:03 +0200

    Sesión 10 - GUI

---
+++ 10_GUI/104_geometria_place.py
@@ -0,0 +1,16 @@
+import tkinter
+from tkinter import ttk
+import random
+
+window = tkinter.Tk()
+
+colors = ['blue', 'red', 'yellow', 'purple', 'green', 'black']
+
+
+for x in range(0, 10):
+    color = random.randint(0, len(colors)-1)
+    color2 = random.randint(0, len(colors)-1)
+    label = tkinter.Label(window, text='etiqueta!', bg=colors[color], fg=colors[color])
+    label.place(x=random.randint(0,100), y=random.randint(0,100))
+
+window.mainloop()
